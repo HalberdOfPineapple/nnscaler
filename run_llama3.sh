b9096927bf17d27789b7657cee56a4d154cb62ab
Wenxuan Li <wl446@cam.ac.uk>
2024-10-24 05:26:58 +0000

    modified gitignore

--- run_llama3.sh
+++ run_llama3.sh
@@ -7,17 +7,17 @@ cd ~/nnscaler/examples/llama3_8B_128K
 mkdir -p /blob/logs/nnscaler/llama_8B_128K_cuda_4
 
 # Execute the first command and redirect output to the data preparation log
-echo "Running data preparation..."
-python bookcorpus.py --data_path_or_name bookcorpus/bookcorpus \
-                    --tokenizer_path_or_name meta-llama/Meta-Llama-3-8B-Instruct \
-                    --save_path /scratch/nnscaler/bookcorpus_llama3_4K \
-                    --sequence_length 4096 \
-                    > /blob/logs/nnscaler/llama_8B_128K_cuda_4/data_prepare.log 2>&1
+# echo "Running data preparation..."
+# python bookcorpus.py --data_path_or_name bookcorpus/bookcorpus \
+#                     --tokenizer_path_or_name meta-llama/Meta-Llama-3-8B-Instruct \
+#                     --save_path /scratch/nnscaler/bookcorpus_llama3_4K \
+#                     --sequence_length 4096 \
+#                     > /blob/logs/nnscaler/llama_8B_128K_cuda_4/data_prepare.log 2>&1
 
-# Execute the second command
-echo "Creating mini model..."
-python create_mini_model.py --model_id meta-llama/Meta-Llama-3-8B-Instruct \
-                            --output_id /scratch/nnscaler/llama3_mini
+# # Execute the second command
+# echo "Creating mini model..."
+# python create_mini_model.py --model_id meta-llama/Meta-Llama-3-8B-Instruct \
+#                             --output_id /scratch/nnscaler/llama3_mini
 
 # Execute the third command and redirect output to the training log
 echo "Starting training..."
