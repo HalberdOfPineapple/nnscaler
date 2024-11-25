(TODO) fix gradient w.r.t K

experiments/minfer_phi/minfer_ops/fa_attn_test.py
@@ -605,10 +605,10 @@ def _mbwd_kernel(
     Out, DO,
     DQ, DK, DV,
     L,
-    D,
+    D, # [BATCH, N_HEADS, N_CTX]
     stride_qz, stride_qh, stride_qm, stride_qk,
     stride_kz, stride_kh, stride_kn, stride_kk,
-    stride_vz, stride_vh, stride_vk, stride_vn,
+    stride_vz, stride_vh, stride_vn, stride_vk, 
     Z, H, N_CTX, P_SEQ,
     num_block_q, num_block_kv,
     BLOCK_M: tl.constexpr, BLOCK_DMODEL: tl.constexpr,
@@ -618,7 +618,8 @@ def _mbwd_kernel(
     off_hz = tl.program_id(0)
     off_z = off_hz // H
     off_h = off_hz % H
-    qk_scale = sm_scale / LN2
+    qk_scale = sm_scale * 1.44269504
+
     # offset pointers for batch/head
     Q += off_z * stride_qz + off_h * stride_qh
     K += off_z * stride_kz + off_h * stride_kh
@@ -628,77 +629,74 @@ def _mbwd_kernel(
     DK += off_z * stride_kz + off_h * stride_kh
     DV += off_z * stride_vz + off_h * stride_vh
 
-    
-    for start_n in range(0, num_block_kv):
-        lo = tl.math.max(start_n * BLOCK_M - P_SEQ, 0) if CAUSAL else 0
-        
+    # loop over rows
+    offs_k = tl.arange(0, BLOCK_DMODEL)
+    for start_m in range(0, num_block_q):
         # initialize row/col offsets
-        offs_qm = lo + tl.arange(0, BLOCK_M)
-
-        offs_n = start_n * BLOCK_M + tl.arange(0, BLOCK_M)
-        offs_m = tl.arange(0, BLOCK_N)
-        offs_k = tl.arange(0, BLOCK_DMODEL)
+        offs_m = start_m * BLOCK_M + tl.arange(0, BLOCK_M)
+        m_mask = offs_m[:, None] < context_size
 
         # initialize pointers to value-like data
-        q_ptrs = Q + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
-        k_ptrs = K + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk)
-        v_ptrs = V + (offs_n[:, None] * stride_qm + offs_k[None, :] * stride_qk)
-        do_ptrs = DO + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
-        dq_ptrs = DQ + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+        q_ptrs  = Q  + (offs_m[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+        do_ptrs = DO + (offs_m[:, None] * stride_qm + offs_k[None, :] * stride_qk)
 
         # pointer to row-wise quantities in value-like data
-        D_ptrs = D + off_hz * N_CTX
-        l_ptrs = L + off_hz * N_CTX
+        D_ptrs = D + off_hz * N_CTX + offs_m
+        l_ptrs = L + off_hz * N_CTX + offs_m
 
-        # initialize dk amd dv
-        dk = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
-        dv = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
-        # k and v stay in SRAM throughout
-        k = tl.load(k_ptrs)
-        v = tl.load(v_ptrs)
-        # loop over rows
-        for start_m in range(lo, num_block_q * BLOCK_M, BLOCK_M):
-            offs_m_curr = start_m + offs_m
-            # load q, k, v, do on-chip
-            q = tl.load(q_ptrs)
-            
+        q = tl.load(q_ptrs)
+        do = tl.load(do_ptrs)
+        l_i = tl.load(l_ptrs)
+        D_i = tl.load(D_ptrs)
+        
+        dq = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
+        
+        # num_block_kv_curr = (start_m * BLOCK_M + BLOCK_N - 1) // BLOCK_N
+        # for start_n in range(0, num_block_kv_curr):
+        for start_n in range(0, num_block_kv):
+            offs_n = start_n * BLOCK_N + tl.arange(0, BLOCK_N)
+
+            # k and v stay in SRAM throughout
+            k_ptrs  = K  + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk)
+            dk_ptrs = DK + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk)
+            k = tl.load(k_ptrs)
+            dk = tl.load(dk_ptrs)
+
+            v_ptrs = V + (offs_n[:, None] * stride_vn + offs_k[None, :] * stride_vk)
+            dv_ptrs = DV + (offs_n[:, None] * stride_vn + offs_k[None, :] * stride_vk)
+            v = tl.load(v_ptrs)
+            dv = tl.load(dv_ptrs)
+
             if CAUSAL:
-                causal_mask = (P_SEQ + offs_m_curr[:, None]) >= (offs_n[None, :])
-                m_mask = offs_m_curr[:, None] < context_size
+                causal_mask = (P_SEQ + offs_m[:, None]) >= (offs_n[None, :])
                 qk = tl.where(causal_mask & m_mask, float(0.), float("-inf"))
             else:
                 qk = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
+
             qk += tl.dot(q, tl.trans(k))
             qk *= qk_scale
-            l_i = tl.load(l_ptrs + offs_m_curr)
             p = tl.math.exp2(qk - l_i[:, None])
 
             # compute dv
-            do = tl.load(do_ptrs)
             dv += tl.dot(tl.trans(p.to(Q.dtype.element_ty)), do)
+            tl.store(dv_ptrs, dv)
 
-            
             # compute dp = dot(v, do)
-            Di = tl.load(D_ptrs + offs_m_curr)
-            dp = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32) - Di[:, None]
+            dp = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32) - D_i[:, None]
             dp += tl.dot(do, tl.trans(v))
+            
             # compute ds = p * (dp - delta[:, None])
             ds = p * dp * sm_scale
+
             # compute dk = dot(ds.T, q)
             dk += tl.dot(tl.trans(ds.to(Q.dtype.element_ty)), q)
+            tl.store(dk_ptrs, dk)
+
             # compute dq
-            dq = tl.load(dq_ptrs)
             dq += tl.dot(ds.to(Q.dtype.element_ty), k)
-            tl.store(dq_ptrs, dq)
-            # increment pointers
-            dq_ptrs += BLOCK_M * stride_qm
-            q_ptrs += BLOCK_M * stride_qm
-            do_ptrs += BLOCK_M * stride_qm
-        # write-back
-        dk_ptrs = DK + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk)
-        dv_ptrs = DV + (offs_n[:, None] * stride_qm + offs_k[None, :] * stride_qk)
-        tl.store(dk_ptrs, dk)
-        tl.store(dv_ptrs, dv)
+
+        dq_ptrs = DQ + (offs_m[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+        tl.store(dq_ptrs, dq)
 
 class MFMB(torch.autograd.Function):
     @staticmethod
@@ -707,6 +705,7 @@ class MFMB(torch.autograd.Function):
         query, key, value, 
         block_count, block_offset, column_count, column_index,
         seqlens: torch.Tensor,
+        causal: bool,
         block_size_M: int = 64,
         block_size_N: int = 64,
     ):
@@ -736,6 +735,7 @@ class MFMB(torch.autograd.Function):
         
         ctx.block_size_M = block_size_M
         ctx.block_size_N = block_size_N
+        ctx.causal = causal
         return o
     
     @staticmethod
@@ -743,6 +743,7 @@ class MFMB(torch.autograd.Function):
         q, k, v, o, L = ctx.saved_tensors
         block_size_M, block_size_N = ctx.block_size_M, ctx.block_size_N
         head_dim, context_size = ctx.head_dim, ctx.context_size
+        causal = ctx.causal
         sm_scale = 1.0 / (head_dim ** 0.5)
 
         print('-' * 20)
@@ -761,11 +762,12 @@ class MFMB(torch.autograd.Function):
             D_HEAD=q.shape[-1],
         )
 
-        # D = torch.sum(do * o, dim=-1).to(L.dtype)
-        # if not torch.allclose(delta, D, atol=ATOL, rtol=RTOL):
-        #     print(f"Triton-calculated Delta is not close to D")
-        # else:
-        #     print(f"Delta correctly calculated")
+        D = torch.sum(do * o, dim=-1).to(L.dtype)
+        if not torch.allclose(delta, D, atol=ATOL, rtol=RTOL):
+            print(f"Triton-calculated Delta is not close to D")
+        else:
+            print(f"Delta correctly calculated")
+        del D
 
         _mbwd_kernel[(grid[1],)](
             q, k, v,
@@ -782,22 +784,31 @@ class MFMB(torch.autograd.Function):
             BLOCK_M=block_size_M, 
             BLOCK_N=block_size_N,
             BLOCK_DMODEL=q.shape[-1], 
-            CAUSAL=True,
+            CAUSAL=causal,
             num_warps=8,
             num_stages=1,
         )
 
 
-        return dq[..., :context_size, :head_dim], dk[..., :context_size, :head_dim], dv[..., :context_size, :head_dim], None, None, None, None, None
+        return dq[..., :context_size, :head_dim], dk[..., :context_size, :head_dim], dv[..., :context_size, :head_dim], None, None, None, None, None, None
 
 
 def main(args):
-    torch.manual_seed(1024)
+    torch.manual_seed(0)
     mode: str = args.mode
+    causal: bool = not args.disable_causal
 
     context_size = args.context_size
     num_heads = 1 # 32
     head_dim = 96
+
+    print('-' * 50)
+    print(f"Mode: {mode}")
+    print(f"Context Size: {context_size}")
+    print(f"Num Heads: {num_heads}")
+    print(f"Head Dim: {head_dim}")
+    print(f"Causal: {causal}")
+    print('-' * 50)
 
     q = torch.randn((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
     k = torch.randn((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
@@ -822,7 +833,7 @@ def main(args):
         v.transpose(1, 2),
         dropout_p=0.0,
         softmax_scale=None,
-        causal=True,
+        causal=causal,
     )
     o_ref = o_ref.transpose(1, 2)
     o_ref.retain_grad()
@@ -860,7 +871,7 @@ def main(args):
             v.transpose(1, 2),
             dropout_p=0.0,
             softmax_scale=None,
-            causal=True,
+            causal=causal,
         )
         o = o.transpose(1, 2)
     elif mode == "mfrb":
@@ -875,7 +886,8 @@ def main(args):
         o = MFMB.apply(
             q, k, v,
             block_count, block_offset, column_count, column_index,
-            seqlens
+            seqlens,
+            causal,
         )
         o = o[..., :, :context_size, :head_dim]
     elif mode == "ref":
@@ -949,5 +961,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--mode', type=str, default='naive')
     parser.add_argument('-n', '--context_size', type=int, default=256)
+    parser.add_argument('-c', '--disable_causal', action='store_true')
     args = parser.parse_args()
     main(args)