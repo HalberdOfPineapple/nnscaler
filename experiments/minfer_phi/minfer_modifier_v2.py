fix mfmb (masking)

experiments/minfer_phi/minfer_modifier_v2.py
@@ -120,7 +120,7 @@ class MInferAttention(PhiAttention):
         # print(f"query_states: {query_states.shape}, key_states: {key_states.shape}, value_states: {value_states.shape}, head_indices: {head_indices.shape}")
         attn_output = attn_fwd_by_heads(
             query_states, key_states, value_states, head_indices,
-            bsz, q_len,  self.head_dim, pattern,
+            bsz, q_len,  self.head_dim, self.layer_idx, pattern,
         ) # expect:  b l^ {q_anno} vd^'
     
         attn_output = attn_output.reshape(bsz, q_len, self.hidden_size).contiguous()
@@ -137,9 +137,9 @@ def attn_fwd_by_heads(
     bsz: int,
     q_len: int,
     head_dim: int,
+    layer_idx: int,
     pattern: Tuple[str, int, int, int],
 ):
-    # print(f"head_indices: {head_indices}")
     with torch.autograd.set_detect_anomaly(True):
         output_list = []
         assert(query_states.shape[1] == head_indices.shape[-1])
@@ -152,10 +152,21 @@ def attn_fwd_by_heads(
             # if search is disabled and the current layer is beyond  starting layer 
             # => apply the kernel for calculating the attention based on the best pattern
             ty, vertical_size, slash_size, _ = pattern
-            attn_output_head = vs_attn_forward(
-                q, k, v,
-                q_len, vertical_size, slash_size, head_dim
-            ).view(bsz, 1, q_len, head_dim)
+
+            try:
+                attn_output_head = vs_attn_forward(
+                    q, k, v,
+                    q_len, vertical_size, slash_size, head_dim,
+                    layer_idx, head_indices[head].item()
+                ).view(bsz, 1, q_len, head_dim)
+            except Exception as e:
+                print(f"Error in (Layer {layer_idx}, Head {head_indices[head].item()}) with pattern {pattern}")
+
+                import traceback
+                traceback.print_exc()
+                
+                attn_output_head = torch.randn(bsz, 1, q_len, head_dim, device=query_states.device)
+
             # output_list.append(attn_output_head.squeeze(2))
             output_list.append(attn_output_head)
 

experiments/minfer_phi/minfer_ops/fa_attn_test/fa_attn_test.py
@@ -13,7 +13,7 @@ from fwd_utils import pad_tensor, set_rep
 from naive import NaiveAttn
 from mfcb import MFCB, MFCBMask
 from mfrb import MFRB, MFRBRow, MFRBRowRe
-from mfmb import MFMB, MFMBTorch
+from mfmb import MFMB, MFMBTorch, MFMBDT
 from fa_ref import attention as RefAttention
 from fa_ref_2 import attention as RefAttention2
 from sparse_attn_ref import sparse_multi_head_attention_backward_reference
@@ -146,6 +146,14 @@ def attn_by_mode(
             causal,
         )
         o = o[..., :, :context_size, :head_dim]
+    elif mode == "mfmb_dt":
+        o = MFMBDT.apply(
+            q, k, v,
+            block_count, block_offset, column_count, column_index,
+            seqlens,
+            causal,
+        )
+        o = o[..., :, :context_size, :head_dim]
     elif mode == "mfmb_torch":
         o = MFMBTorch.apply(
             q, k, v,
@@ -207,24 +215,24 @@ def main(args):
     print(f"ATOL = {ATOL}, RTOL = {RTOL}")
     print('-' * 50)
 
-    q = torch.randn((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
-    k = torch.randn((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
-    v = torch.randn((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
+    # q = torch.randn((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
+    # k = torch.randn((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
+    # v = torch.randn((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
 
-    # q = torch.ones((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
-    # k = torch.ones((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
-    # v = torch.ones((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
+    q = torch.ones((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
+    k = torch.ones((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
+    v = torch.ones((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
 
     # --------------------------------------------------------------------------------
     # Generate block indices
     block_count, block_offset, column_count, column_index, seqlens = gen_block_indices(
         q, k, v, context_size, vertical_size, slash_size, head_dim
     )
-    # print('-' * 50)
-    # print(f"Block Count:\n{block_count}")
-    # print(f"Block Offset:\n{block_offset}")
-    # print(f"Column Count:\n{column_count}")
-    # print(f"Column Index:\n{column_index}")
+    print('-' * 50)
+    print(f"Block Count:\n{block_count}")
+    print(f"Block Offset:\n{block_offset}")
+    print(f"Column Count:\n{column_count}")
+    print(f"Column Index:\n{column_index}")
 
     # --------------------------------------------------------------------------------
     # Standard FA Library for reference

experiments/minfer_phi/minfer_ops/fa_attn_test/mfmb.py
@@ -33,6 +33,9 @@ def _mbwd_kernel(
 ):
     start_m = tl.program_id(0)
     off_hz = tl.program_id(1)
+    if start_m >= num_block_q or off_hz >= H * Z:
+        return
+
     off_z = off_hz // H
     off_h = off_hz % H
     qk_scale = sm_scale * 1.44269504
@@ -76,12 +79,13 @@ def _mbwd_kernel(
     for block_index in range(num_blks):
         start_n = tl.load(blks_ptr + block_index) # load the start (block-level) index of the non-sparse block
         cols = start_n + offs_n # the indices of elements in the non-sparse block of K, V 
+        n_mask = cols < context_size
 
         # -- load k, v --
-        k_ptrs  = K + (cols[:, None] * stride_kn + offs_k[None, :] * stride_kk)
+        k_ptrs = K + (cols[:, None] * stride_kn + offs_k[None, :] * stride_kk)
         v_ptrs = V + (cols[:, None] * stride_vn + offs_k[None, :] * stride_vk)
-        k = tl.load(k_ptrs)
-        v = tl.load(v_ptrs)
+        k = tl.load(k_ptrs, mask=n_mask[:, None], other=0.)
+        v = tl.load(v_ptrs, mask=n_mask[:, None], other=0.)
 
         if CAUSAL:
             causal_mask = (P_SEQ + offs_m[:, None]) >= (cols[None, :])
@@ -96,7 +100,7 @@ def _mbwd_kernel(
         # compute dv
         dv_ptrs = DV + (cols[:, None] * stride_vn + offs_k[None, :] * stride_vk)
         dv_vals = tl.dot(tl.trans(p.to(Q.dtype.element_ty)), do)
-        tl.atomic_add(dv_ptrs, dv_vals)
+        tl.atomic_add(dv_ptrs, dv_vals, mask=n_mask[:, None])
 
         # compute dp = dot(v, do)
         dp = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32) - D_i[:, None]
@@ -108,7 +112,7 @@ def _mbwd_kernel(
         # compute dk = dot(ds.T, q)
         dk_ptrs = DK + (cols[:, None] * stride_kn + offs_k[None, :] * stride_kk)
         dk_vals = tl.dot(tl.trans(ds.to(Q.dtype.element_ty)), q)
-        tl.atomic_add(dk_ptrs, dk_vals)
+        tl.atomic_add(dk_ptrs, dk_vals, mask=n_mask[:, None])
 
         # compute dq
         dq += tl.dot(ds.to(Q.dtype.element_ty), k)
@@ -135,7 +139,7 @@ def _mbwd_kernel(
         # compute dv
         dv_ptrs = DV + (cols[:, None] * stride_vn + offs_k[None] * stride_vk)
         dv_vals = tl.dot(tl.trans(p.to(Q.dtype.element_ty)), do)
-        tl.atomic_add(dv_ptrs, dv_vals)
+        tl.atomic_add(dv_ptrs, dv_vals, mask=n_mask[:, None])
 
         # compute dp = dot(v, do)
         dp = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32) - D_i[:, None]
@@ -147,13 +151,13 @@ def _mbwd_kernel(
         # compute dk = dot(ds.T, q)
         dk_ptrs = DK + (cols[:, None] * stride_kn + offs_k[None, :] * stride_kk)
         dk_vals = tl.dot(tl.trans(ds.to(Q.dtype.element_ty)), q)
-        tl.atomic_add(dk_ptrs, dk_vals)
+        tl.atomic_add(dk_ptrs, dk_vals, mask=n_mask[:, None])
 
         # compute dq
         dq = dq + tl.dot(ds.to(Q.dtype.element_ty), k)
 
     dq_ptrs = DQ + (offs_m[:, None] * stride_qm + offs_k[None, :] * stride_qk)
-    tl.store(dq_ptrs, dq)
+    tl.store(dq_ptrs, dq, mask=m_mask)
 
 class MFMB(torch.autograd.Function):
     @staticmethod
@@ -241,13 +245,20 @@ class MFMB(torch.autograd.Function):
         dv = torch.zeros_like(v)
         delta = torch.zeros_like(L)
 
+        torch.cuda.synchronize()
+        print(f"{__class__.__name__} | Starting Preprocess Kernel...", end=" ") 
         _bwd_preprocess[(grid[0] * grid[1], )](
             o, do,
             delta,
             BLOCK_M=block_size_M, 
             D_HEAD=q.shape[-1],
         )
+        print("Done.")
+        torch.cuda.synchronize()
 
+
+        torch.cuda.synchronize()
+        print(f"{__class__.__name__} | Starting Triton Kernel...", end=" ")
         _mbwd_kernel[(grid[0], grid[1],)](
             q, k, v,
             sm_scale,
@@ -269,6 +280,9 @@ class MFMB(torch.autograd.Function):
             num_warps=8,
             num_stages=2,
         )
+        print("Done.")
+        torch.cuda.synchronize()
+
 
         return dq[..., :context_size, :head_dim], dk[..., :context_size, :head_dim], dv[..., :context_size, :head_dim], None, None, None, None, None, None
 
@@ -403,7 +417,7 @@ class MFMBTorch(torch.autograd.Function):
                         k_block, v_block = k[cols, :], v[cols, :]
                         print(f"rows = {rows}, cols = {cols}")
                         # print(f"{cols}, ", end="")
-                        
+
                         qk = torch.zeros([len(rows), len(cols)]).to(query.device)
 
                         qk += torch.matmul(q_block, k_block.transpose(-1, -2))
@@ -424,4 +438,279 @@ class MFMBTorch(torch.autograd.Function):
                     DQ[batch_idx, head_idx, rows, :] = dq
 
         return DQ[..., :context_size, :head_dim], DK[..., :context_size, :head_dim], DV[..., :context_size, :head_dim], None, None, None, None, None, None
+
+
+
+@triton.jit
+def _mbwd_dt_kernel(
+    Q, K, V, 
+    sm_scale, context_size,
+    block_count, # [BATCH, N_HEADS, NUM_ROWS], note that NUM_ROWS means the number of 64-sized rows
+    block_offset, # [BATCH, N_HEADS, NUM_ROWS, NNZ_S], which refers to the start of the non-sparse K/V blocks to be computed with the corresponding Q block
+    column_count, # [BATCH, N_HEADS, NUM_ROWS]
+    column_index, # [BATCH, N_HEADS, NUM_ROWS, NNZ_V]
+    Out, DO,
+    DQ, DK, DV,
+    L,
+    D, # [BATCH, N_HEADS, N_CTX]
+    stride_qz, stride_qh, stride_qm, stride_qk,
+    stride_kz, stride_kh, stride_kn, stride_kk,
+    stride_vz, stride_vh, stride_vn, stride_vk, 
+    stride_dqz, stride_dqh, stride_dqm, stride_dqk,
+    stride_dkz, stride_dkh, stride_dkn, stride_dkk,
+    stride_dvz, stride_dvh, stride_dvn, stride_dvk,
+    Z, H, N_CTX, P_SEQ,
+    num_block_q, num_block_kv, NNZ_S, NNZ_V,
+    BLOCK_M: tl.constexpr, BLOCK_DMODEL: tl.constexpr,
+    BLOCK_N: tl.constexpr,
+    CAUSAL: tl.constexpr,
+):
+    
+    start_m = tl.program_id(0)
+    off_hz = tl.program_id(1)
+    if start_m >= num_block_q or off_hz >= H * Z:
+        return
+
+
+    off_z = off_hz // H
+    off_h = off_hz % H
+    qk_scale = sm_scale * 1.44269504
+
+    # offset pointers for batch/head
+    Q += off_z * stride_qz + off_h * stride_qh
+    K += off_z * stride_kz + off_h * stride_kh
+    V += off_z * stride_vz + off_h * stride_vh
+    DO += off_z * stride_qz + off_h * stride_qh
+
+    DQ += off_z * stride_dqz + off_h * stride_dqh
+    DK += off_z * stride_dkz + off_h * stride_dkh
+    DV += off_z * stride_dvz + off_h * stride_dvh
+
+    # loop over rows
+    offs_k = tl.arange(0, BLOCK_DMODEL)
+    offs_m = start_m * BLOCK_M + tl.arange(0, BLOCK_M)
+    offs_n = tl.arange(0, BLOCK_N)
+    m_mask = offs_m[:, None] < context_size
+
+    # initialize pointers to value-like data
+    q_ptrs  = Q  + (offs_m[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+    do_ptrs = DO + (offs_m[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+
+    # pointer to row-wise quantities in value-like data
+    D_ptrs = D + off_hz * N_CTX + offs_m
+    l_ptrs = L + off_hz * N_CTX + offs_m
+
+    q = tl.load(q_ptrs)
+    do = tl.load(do_ptrs)
+    l_i = tl.load(l_ptrs)
+    D_i = tl.load(D_ptrs)
+
+    dq = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
+
+    num_blks = tl.load(block_count + off_hz * num_block_q + start_m) # load the number of non-sparse blocks
+    blks_ptr = block_offset + (off_hz * num_block_q + start_m) * NNZ_S # pointer to the start of the list of non-sparse blocks
+    
+    num_cols = tl.load(column_count + off_hz * num_block_q + start_m) # load the number of non-sparse column(block)s
+    cols_ptr = column_index + (off_hz * num_block_q + start_m) * NNZ_V 
+
+    for block_index in range(num_blks):
+        start_n = tl.load(blks_ptr + block_index) # load the start (block-level) index of the non-sparse block
+        cols = start_n + offs_n # the indices of elements in the non-sparse block of K, V 
+        n_mask = cols[:, None] < context_size
+
+        # -- load k, v --
+        k_ptrs = K + (cols[:, None] * stride_kn + offs_k[None, :] * stride_kk)
+        v_ptrs = V + (cols[:, None] * stride_vn + offs_k[None, :] * stride_vk)
+        k = tl.load(k_ptrs, mask=n_mask, other=0.)
+        v = tl.load(v_ptrs, mask=n_mask, other=0.)
+
+        if CAUSAL:
+            causal_mask = (P_SEQ + offs_m[:, None]) >= (cols[None, :])
+            qk = tl.where(causal_mask & m_mask, float(0.), float("-inf")).to(tl.float32)
+        else:
+            qk = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32) 
+
+        qk = qk + tl.dot(q, tl.trans(k))
+        qk = qk * qk_scale
+        p = tl.math.exp2(qk - l_i[:, None])
+
+        # compute dv
+        dv_ptrs = DV + (cols[:, None] * stride_dvn + offs_k[None, :] * stride_dvk)
+        dv_vals = tl.dot(tl.trans(p.to(Q.dtype.element_ty)), do)
+        tl.atomic_add(dv_ptrs, dv_vals.to(DV.dtype.element_ty), mask=n_mask)
+
+        # compute dp = dot(v, do)
+        dp = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32) - D_i[:, None]
+        dp += tl.dot(do, tl.trans(v))
+
+        # compute ds = p * (dp - delta[:, None])
+        ds = p * dp * sm_scale
+
+        # compute dk = dot(ds.T, q)
+        dk_ptrs = DK + (cols[:, None] * stride_dkn + offs_k[None, :] * stride_dkk)
+        dk_vals = tl.dot(tl.trans(ds.to(Q.dtype.element_ty)), q)
+        tl.atomic_add(dk_ptrs, dk_vals.to(DK.dtype.element_ty), mask=n_mask)
+        # compute dq
+        dq = dq + tl.dot(ds.to(Q.dtype.element_ty), k)
+
+    
+    for start_col_idx in range(0, num_cols, BLOCK_N):
+        # the key difference is that cols, as the indices, are stored in and loaded from cols_ptr
+        # At each iteration, a block-sized chunk of column **indices** are loaded from cols_ptr, which can be discontinuous
+        # But we just load the indices block by block, equivalent to translating the non-sparse columns together
+        n_mask = (start_col_idx + offs_n < num_cols)
+        cols = tl.load(cols_ptr + start_col_idx + offs_n, mask=n_mask, other=0.)
+
+
+        # -- load k, v --
+        k_ptrs = K + (cols[:, None] * stride_kn + offs_k[None, :] * stride_kk)
+        v_ptrs = V + (cols[:, None] * stride_vn + offs_k[None, :] * stride_vk)
+        k = tl.load(k_ptrs, mask=n_mask[:, None], other=0.)
+        v = tl.load(v_ptrs, mask=n_mask[:, None], other=0.)
+
+        # Computer qk
+        qk = tl.where(m_mask & n_mask[None, :], float(0.), float("-inf"))
+        qk = qk + tl.dot(q, tl.trans(k))
+        qk = qk * qk_scale
+        p = tl.math.exp2(qk - l_i[:, None])
+
+        # compute dv
+        dv_ptrs = DV + (cols[:, None] * stride_dvn + offs_k[None, :] * stride_dvk)
+        dv_vals = tl.dot(tl.trans(p.to(Q.dtype.element_ty)), do)
+        tl.atomic_add(dv_ptrs, dv_vals, mask=n_mask[:, None])
+
+        # compute dp = dot(v, do)
+        dp = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32) - D_i[:, None]
+        dp = dp + tl.dot(do, tl.trans(v))
+        
+        # compute ds = p * (dp - delta[:, None])
+        ds = p * dp * sm_scale
+
+        # compute dk = dot(ds.T, q)
+        dk_ptrs = DK + (cols[:, None] * stride_dkn + offs_k[None, :] * stride_dkk)
+        dk_vals = tl.dot(tl.trans(ds.to(Q.dtype.element_ty)), q)
+        tl.atomic_add(dk_ptrs, dk_vals, mask=n_mask[:, None])
+
+        # compute dq
+        dq = dq + tl.dot(ds.to(Q.dtype.element_ty), k)
+    
+    dq_ptrs = DQ + (offs_m[:, None] * stride_dqm + offs_k[None, :] * stride_dqk)
+    tl.store(dq_ptrs, dq.to(DQ.dtype.element_ty), mask=m_mask)
+
+class MFMBDT(torch.autograd.Function):
+    @staticmethod
+    def forward(
+        ctx, 
+        query, key, value, 
+        block_count, block_offset, column_count, column_index,
+        seqlens: torch.Tensor,
+        causal: bool,
+        block_size_M: int = 64,
+        block_size_N: int = 64,
+    ):
+        _,  _, context_size, head_dim = query.shape
+        ctx.context_size = context_size
+        ctx.head_dim = head_dim
+
+        # the input version of Q, K and V are padded to be divisible by BLOCK_SIZE_M
+        # the output and softmax_lse are also padded
+        sm_scale = head_dim ** -0.5
+
+        # Check column_index
+        if torch.any(column_index < 0) or torch.any(column_index >= context_size):
+            raise ValueError("Ilegal column index detected.")
+        else:
+            print("Column Index is legal.")
+
+        query, key, value = (
+            pad_tensor(query, context_size, head_dim, block_size_M), 
+            pad_tensor(key, context_size, head_dim, block_size_M), 
+            pad_tensor(value, context_size, head_dim, block_size_M)
+        )
+
+        o, softmax_lse = _triton_mixed_sparse_attention(
+            query, key, value, seqlens,
+            block_count, block_offset, column_count, column_index,
+            sm_scale, context_size, head_dim,
+            block_size_M, block_size_N,
+        )
+
+        ctx.save_for_backward(
+            query, key, value, o, softmax_lse,
+            block_count, block_offset, column_count, column_index,
+        )
+        
+        ctx.block_size_M = block_size_M
+        ctx.block_size_N = block_size_N
+        ctx.causal = causal
+        return o
+
+    @staticmethod
+    def backward(ctx, do):
+        (
+            q, k, v, o, L,
+            block_count, block_offset, column_count, column_index
+        ) = ctx.saved_tensors
+        block_size_M, block_size_N = ctx.block_size_M, ctx.block_size_N
+        head_dim, context_size = ctx.head_dim, ctx.context_size
+        causal = ctx.causal
+        sm_scale = 1.0 / (head_dim ** 0.5)
+
+        grid = (triton.cdiv(q.shape[2], block_size_M), q.shape[0] * q.shape[1], 1)
+
+        do = do.contiguous()
+        dq, dk, dv = (
+            torch.zeros_like(q).to(torch.float32), 
+            torch.zeros_like(k).to(torch.float32), 
+            torch.zeros_like(v).to(torch.float32)
+        )
+        delta = torch.zeros_like(L)
+
+        torch.cuda.synchronize()
+        print(f"{__class__.__name__} | Starting Preprocess Kernel...", end=" ") 
+        _bwd_preprocess[(grid[0] * grid[1], )](
+            o, do,
+            delta,
+            BLOCK_M=block_size_M, 
+            D_HEAD=q.shape[-1],
+        )
+        print("Done.")
+        torch.cuda.synchronize()
+
+        torch.cuda.synchronize()
+        print(f"{__class__.__name__} | Starting Triton Kernel...", end=" ") 
+        _mbwd_dt_kernel[(grid[0], grid[1],)](
+            q, k, v,
+            sm_scale, context_size,
+            block_count, block_offset, column_count, column_index,
+            o, do,
+            dq, dk, dv,
+            L, delta,
+            q.stride(0), q.stride(1), q.stride(2), q.stride(3),
+            k.stride(0), k.stride(1), k.stride(2), k.stride(3),
+            v.stride(0), v.stride(1), v.stride(2), v.stride(3),
+
+            dq.stride(0), dq.stride(1), dq.stride(2), dq.stride(3),
+            dk.stride(0), dk.stride(1), dk.stride(2), dk.stride(3),
+            dv.stride(0), dv.stride(1), dv.stride(2), dv.stride(3),
+
+            q.shape[0], q.shape[1], q.shape[2], 0,
+            grid[0], triton.cdiv(k.shape[2], block_size_N), 
+            block_offset.shape[-1], column_index.shape[-1],
+            BLOCK_M=block_size_M, 
+            BLOCK_N=block_size_N,
+            BLOCK_DMODEL=q.shape[-1], 
+            CAUSAL=causal,
+            num_warps=32,
+            num_stages=2,
+        )
+        print("Done.")
+        torch.cuda.synchronize()
+
+        return (
+            dq[:, :, :context_size, :head_dim].to(q.dtype), 
+            dk[:, :, :context_size, :head_dim].to(k.dtype), 
+            dv[:, :, :context_size, :head_dim].to(v.dtype), 
+            None, None, None, None, None, None
+        )
 

experiments/minfer_phi/minfer_ops/pit_flash_attn.py
@@ -113,6 +113,9 @@ def _mbwd_kernel(
     stride_qz, stride_qh, stride_qm, stride_qk,
     stride_kz, stride_kh, stride_kn, stride_kk,
     stride_vz, stride_vh, stride_vn, stride_vk, 
+    stride_dqz, stride_dqh, stride_dqm, stride_dqk,
+    stride_dkz, stride_dkh, stride_dkn, stride_dkk,
+    stride_dvz, stride_dvh, stride_dvn, stride_dvk,
     Z, H, N_CTX, P_SEQ,
     num_block_q, num_block_kv, NNZ_S, NNZ_V,
     BLOCK_M: tl.constexpr, BLOCK_DMODEL: tl.constexpr,
@@ -121,6 +124,9 @@ def _mbwd_kernel(
 ):
     start_m = tl.program_id(0)
     off_hz = tl.program_id(1)
+    if start_m >= num_block_q or off_hz >= H * Z:
+        return
+
     off_z = off_hz // H
     off_h = off_hz % H
     qk_scale = sm_scale * 1.44269504
@@ -130,9 +136,10 @@ def _mbwd_kernel(
     K += off_z * stride_kz + off_h * stride_kh
     V += off_z * stride_vz + off_h * stride_vh
     DO += off_z * stride_qz + off_h * stride_qh
-    DQ += off_z * stride_qz + off_h * stride_qh
-    DK += off_z * stride_kz + off_h * stride_kh
-    DV += off_z * stride_vz + off_h * stride_vh
+
+    DQ += off_z * stride_dqz + off_h * stride_dqh
+    DK += off_z * stride_dkz + off_h * stride_dkh
+    DV += off_z * stride_dvz + off_h * stride_dvh
 
     # loop over rows
     offs_k = tl.arange(0, BLOCK_DMODEL)
@@ -164,27 +171,28 @@ def _mbwd_kernel(
     for block_index in range(num_blks):
         start_n = tl.load(blks_ptr + block_index) # load the start (block-level) index of the non-sparse block
         cols = start_n + offs_n # the indices of elements in the non-sparse block of K, V 
+        n_mask = cols[:, None] < context_size
 
         # -- load k, v --
-        k_ptrs  = K + (cols[:, None] * stride_kn + offs_k[None, :] * stride_kk)
+        k_ptrs = K + (cols[:, None] * stride_kn + offs_k[None, :] * stride_kk)
         v_ptrs = V + (cols[:, None] * stride_vn + offs_k[None, :] * stride_vk)
-        k = tl.load(k_ptrs)
-        v = tl.load(v_ptrs)
+        k = tl.load(k_ptrs, mask=n_mask, other=0.)
+        v = tl.load(v_ptrs, mask=n_mask, other=0.)
 
         if CAUSAL:
             causal_mask = (P_SEQ + offs_m[:, None]) >= (cols[None, :])
             qk = tl.where(causal_mask & m_mask, float(0.), float("-inf")).to(tl.float32)
         else:
-            qk = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
+            qk = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32) 
 
-        qk += tl.dot(q, tl.trans(k))
-        qk *= qk_scale
+        qk = qk + tl.dot(q, tl.trans(k))
+        qk = qk * qk_scale
         p = tl.math.exp2(qk - l_i[:, None])
 
         # compute dv
-        dv_ptrs = DV + (cols[:, None] * stride_vn + offs_k[None, :] * stride_vk)
+        dv_ptrs = DV + (cols[:, None] * stride_dvn + offs_k[None, :] * stride_dvk)
         dv_vals = tl.dot(tl.trans(p.to(Q.dtype.element_ty)), do)
-        tl.atomic_add(dv_ptrs, dv_vals)
+        tl.atomic_add(dv_ptrs, dv_vals.to(DV.dtype.element_ty), mask=n_mask)
 
         # compute dp = dot(v, do)
         dp = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32) - D_i[:, None]
@@ -194,19 +202,19 @@ def _mbwd_kernel(
         ds = p * dp * sm_scale
 
         # compute dk = dot(ds.T, q)
-        dk_ptrs = DK + (cols[:, None] * stride_kn + offs_k[None, :] * stride_kk)
+        dk_ptrs = DK + (cols[:, None] * stride_dkn + offs_k[None, :] * stride_dkk)
         dk_vals = tl.dot(tl.trans(ds.to(Q.dtype.element_ty)), q)
-        tl.atomic_add(dk_ptrs, dk_vals)
-
+        tl.atomic_add(dk_ptrs, dk_vals.to(DK.dtype.element_ty), mask=n_mask)
         # compute dq
-        dq += tl.dot(ds.to(Q.dtype.element_ty), k)
+        dq = dq + tl.dot(ds.to(Q.dtype.element_ty), k)
 
-    for start_n in range(0, num_cols, BLOCK_N):
+    
+    for start_col_idx in range(0, num_cols, BLOCK_N):
         # the key difference is that cols, as the indices, are stored in and loaded from cols_ptr
         # At each iteration, a block-sized chunk of column **indices** are loaded from cols_ptr, which can be discontinuous
         # But we just load the indices block by block, equivalent to translating the non-sparse columns together
-        n_mask = (start_n + offs_n < num_cols)
-        cols = tl.load(cols_ptr + start_n + offs_n, mask=n_mask, other=0)
+        n_mask = (start_col_idx + offs_n < num_cols)
+        cols = tl.load(cols_ptr + start_col_idx + offs_n, mask=n_mask, other=0.)
 
         # -- load k, v --
         k_ptrs = K + (cols[:, None] * stride_kn + offs_k[None, :] * stride_kk)
@@ -215,15 +223,15 @@ def _mbwd_kernel(
         v = tl.load(v_ptrs, mask=n_mask[:, None], other=0.)
 
         # Computer qk
-        qk = tl.where(m_mask & n_mask, float(0.), float("-inf"))
+        qk = tl.where(m_mask & n_mask[None, :], float(0.), float("-inf"))
         qk = qk + tl.dot(q, tl.trans(k))
         qk = qk * qk_scale
         p = tl.math.exp2(qk - l_i[:, None])
 
         # compute dv
-        dv_ptrs = DV + (cols[:, None] * stride_vn + offs_k[None] * stride_vk)
+        dv_ptrs = DV + (cols[:, None] * stride_dvn + offs_k[None, :] * stride_dvk)
         dv_vals = tl.dot(tl.trans(p.to(Q.dtype.element_ty)), do)
-        tl.atomic_add(dv_ptrs, dv_vals)
+        tl.atomic_add(dv_ptrs, dv_vals, mask=n_mask[:, None])
 
         # compute dp = dot(v, do)
         dp = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32) - D_i[:, None]
@@ -233,17 +241,15 @@ def _mbwd_kernel(
         ds = p * dp * sm_scale
 
         # compute dk = dot(ds.T, q)
-        dk_ptrs = DK + (cols[:, None] * stride_kn + offs_k[None, :] * stride_kk)
+        dk_ptrs = DK + (cols[:, None] * stride_dkn + offs_k[None, :] * stride_dkk)
         dk_vals = tl.dot(tl.trans(ds.to(Q.dtype.element_ty)), q)
-        tl.atomic_add(dk_ptrs, dk_vals)
+        tl.atomic_add(dk_ptrs, dk_vals, mask=n_mask[:, None])
 
         # compute dq
         dq = dq + tl.dot(ds.to(Q.dtype.element_ty), k)
-
-    dq_ptrs = DQ + (offs_m[:, None] * stride_qm + offs_k[None, :] * stride_qk)
-    tl.store(dq_ptrs, dq)
-
-
+    
+    dq_ptrs = DQ + (offs_m[:, None] * stride_dqm + offs_k[None, :] * stride_dqk)
+    tl.store(dq_ptrs, dq.to(DQ.dtype.element_ty), mask=m_mask)
 
 def pad_tensor(x, context_size, head_dim, block_size):
     # Pad to context_length dimension (N_CTX) to be divisible by BLOCK_SIZE_M
@@ -467,12 +473,15 @@ class VSSAttention(torch.autograd.Function):
         query, key, value, 
         block_count, block_offset, column_count, column_index,
         seqlens: torch.Tensor,
+        layer_idx: int, head_idx: int,
         block_size_M: int = 64,
         block_size_N: int = 64,
     ):
         _,  _, context_size, head_dim = query.shape
         ctx.context_size = context_size
         ctx.head_dim = head_dim
+        ctx.layer_idx = layer_idx
+        ctx.head_idx = head_idx
 
         # the input version of Q, K and V are padded to be divisible by BLOCK_SIZE_M
         # the output and softmax_lse are also padded
@@ -507,6 +516,7 @@ class VSSAttention(torch.autograd.Function):
 
         # the original context_size and head_dim
         context_size, head_dim = ctx.context_size, ctx.head_dim
+        layer_idx, head_idx = ctx.layer_idx, ctx.head_idx
         block_size_M, block_size_N = ctx.block_size_M, ctx.block_size_N
         sm_scale = head_dim ** (-0.5)
 
@@ -517,9 +527,9 @@ class VSSAttention(torch.autograd.Function):
         ) = ctx.saved_tensors
         do = pad_tensor(do, context_size, head_dim, block_size_M).contiguous()
         dq, dk, dv = (
-            torch.zeros_like(q).to(torch.float16), 
-            torch.zeros_like(k).to(torch.float16), 
-            torch.zeros_like(v).to(torch.float16)
+            torch.zeros_like(q).to(torch.float32), 
+            torch.zeros_like(k).to(torch.float32), 
+            torch.zeros_like(v).to(torch.float32)
         )
         delta = torch.zeros_like(softmax_lse)
 
@@ -532,33 +542,45 @@ class VSSAttention(torch.autograd.Function):
             D_HEAD=q.shape[-1],
         )
 
-        _mbwd_kernel[(grid[0], grid[1],)](
-            q, k, v,
-            sm_scale,
-            context_size,
-            block_count, block_offset, column_count, column_index,
-            o, do,
-            dq, dk, dv,
-            softmax_lse, delta,
-            q.stride(0), q.stride(1), q.stride(2), q.stride(3),
-            k.stride(0), k.stride(1), k.stride(2), k.stride(3),
-            v.stride(0), v.stride(1), v.stride(2), v.stride(3),
-            q.shape[0], q.shape[1], q.shape[2], 0,
-            grid[0], triton.cdiv(k.shape[2], block_size_N), 
-            block_offset.shape[-1], column_index.shape[-1],
-            BLOCK_M=block_size_M, 
-            BLOCK_N=block_size_N,
-            BLOCK_DMODEL=q.shape[-1], 
-            CAUSAL=True,
-            num_warps=8,
-            num_stages=2,
-        )
+        try:
+            _mbwd_kernel[(grid[0], grid[1],)](
+                q, k, v,
+                sm_scale,
+                context_size,
+                block_count, block_offset, column_count, column_index,
+                o, do,
+                dq, dk, dv,
+                softmax_lse, delta,
+                q.stride(0), q.stride(1), q.stride(2), q.stride(3),
+                k.stride(0), k.stride(1), k.stride(2), k.stride(3),
+                v.stride(0), v.stride(1), v.stride(2), v.stride(3),
+
+                dq.stride(0), dq.stride(1), dq.stride(2), dq.stride(3),
+                dk.stride(0), dk.stride(1), dk.stride(2), dk.stride(3),
+                dv.stride(0), dv.stride(1), dv.stride(2), dv.stride(3),
+
+                q.shape[0], q.shape[1], q.shape[2], 0,
+                grid[0], triton.cdiv(k.shape[2], block_size_N), 
+                block_offset.shape[-1], column_index.shape[-1],
+                BLOCK_M=block_size_M, 
+                BLOCK_N=block_size_N,
+                BLOCK_DMODEL=q.shape[-1], 
+                CAUSAL=True,
+                num_warps=8,
+                num_stages=2,
+            )
+            print('Done')
+        except Exception as e:
+            print(f"Error in (Layer {layer_idx}, Head {head_idx})")
+
+            import traceback
+            traceback.print_exc()
 
         return (
             dq[:, :, :context_size, :head_dim].to(q.dtype), 
             dk[:, :, :context_size, :head_dim].to(k.dtype), 
             dv[:, :, :context_size, :head_dim].to(v.dtype), 
-            None, None, None, None, None
+            None, None, None, None, None, None, None
         )
 
 
@@ -567,6 +589,7 @@ def vs_attn_forward(
     k: torch.Tensor, # [BATCH, N_HEADS, N_CTX, D_HEAD]
     v: torch.Tensor, # [BATCH, N_HEADS, N_CTX, D_HEAD]
     q_len: int, vertical_size: int, slash_size: int, head_dim: int,
+    layer_idx: int, head_idx: int,
     block_size_M: int = 64,
     block_size_N: int = 64,
 ):
@@ -575,7 +598,11 @@ def vs_attn_forward(
         block_size_M, block_size_N,
     )
 
-    return VSSAttention.apply(q, k, v, block_count, block_offset, column_count, column_index, seqlens)
+    return VSSAttention.apply(
+        q, k, v, 
+        block_count, block_offset, column_count, column_index, seqlens,
+        layer_idx, head_idx,
+    )
 
 def test_dense_pattern():
     ATOL, RTOL = 5e-2, 5e-2
@@ -586,10 +613,15 @@ def test_dense_pattern():
 
     vertical_size = 100
     slash_size = context_size
+    # slash_size = 256
 
     q = torch.randn((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
     k = torch.randn((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
     v = torch.randn((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
+
+    # q = torch.ones((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
+    # k = torch.ones((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
+    # v = torch.ones((1, num_heads, context_size, head_dim), dtype=torch.float16, device='cuda', requires_grad=True)
 
     o: torch.Tensor = vs_attn_forward(
         q, k, v, context_size, 